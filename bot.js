7bdd8f6cfe9e4bbd0408c1a9621007a07ca086c0
mhry7r <mi8ho7575@gmail.com>
2026-04-22 19:40:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const gifs = require('./gifs.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
+});
 
 function randomGif(category) {
   const pool = gifs[category].urls;
@@ -18,32 +20,44 @@ client.on('interactionCreate', async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
   const { commandName } = interaction;
-
   if (!gifs[commandName]) return;
 
-  try {
-    const gif = randomGif(commandName);
-    if (!gif) return interaction.reply({ content: '😅 No GIFs found!', ephemeral: true });
+  const gif = randomGif(commandName);
+  if (!gif) return interaction.reply({ content: '😅 No GIFs found!', ephemeral: true });
 
-    const target = interaction.options.getUser('user');
-    const tagger = interaction.user.username;
+  const target = interaction.options.getUser('user');
+  const taggerMember = interaction.member;
+  const tagger = taggerMember?.nickname || interaction.user.displayName || interaction.user.username;
 
-    let message;
-    if (target) {
-      const template = gifs[commandName].message;
-      message = template
-        .replace('{tagger}', `**${tagger}**`)
-        .replace('{tagged}', `**${target.username}**`);
-    } else {
-      message = `**${tagger}** used /${commandName}!`;
-    }
+  let message;
+  if (target) {
+    const targetMember = await interaction.guild.members.fetch(target.id).catch(() => null);
+    const targetName = targetMember?.nickname || target.displayName || target.username;
 
-    await interaction.reply({ content: `${message}\n${gif}` });
-  } catch (err) {
-    console.error(`Error handling /${commandName}:`, err);
-    if (!interaction.replied) {
-      await interaction.reply({ content: '😅 Something went wrong!', ephemeral: true });
-    }
+    message = gifs[commandName].message
+      .replace('{tagger}', `**${tagger}**`)
+      .replace('{tagged}', `**${targetName}** (<@${target.id}>)`);
+  } else {
+    message = gifs[commandName].message
+      .replace('{tagger}', `**${tagger}**`)
+      .replace('{tagged}', '**everyone**');
+  }
+
+  const isLocal = !gif.startsWith('http');
+
+  if (isLocal) {
+    const { AttachmentBuilder } = require('discord.js');
+    const attachment = new AttachmentBuilder(gif);
+    const filename = gif.split('/').pop();
+    const gifEmbed = new EmbedBuilder()
+      .setDescription(message)
+      .setImage(`attachment://${filename}`);
+    await interaction.reply({ files: [attachment], embeds: [gifEmbed] });
+  } else {
+    const gifEmbed = new EmbedBuilder()
+      .setDescription(message)
+      .setImage(gif);
+    await interaction.reply({ embeds: [gifEmbed] });
   }
 });
 
